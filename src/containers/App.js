4076a9e5a72575a4c57489348d26008ed4e05407
Adam <rumpelteam@gmail.com>
2020-05-24 20:31:11 +0200

    Clean project

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './App.module.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className={ classes.Header }>Hello Friends!</h1>
+    </div>
+  );
+}
+
+export default App;
