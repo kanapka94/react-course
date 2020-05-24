d8dcaacf3720f2e0d4c2bd3b93ecfdc93e28a7ce
Adam <rumpelteam@gmail.com>
2020-05-24 20:54:00 +0200

    Add amazing prettier

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 import classes from './App.module.scss'
 
 class App extends Component {
-
-  render () {
+  render() {
     return (
       <div className="App">
-        <h1 className={ classes.Header }>Hello Friends!</h1>
+        <h1 className={classes.Header}>Hello Friends!</h1>
       </div>
     )
   }
 }
 
-export default App;
+export default App
