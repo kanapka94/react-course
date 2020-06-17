387b52515d8127b60dc4d65f7df23796f207a86f
Adam <rumpelteam@gmail.com>
2020-06-17 07:47:44 +0200

    Add prettier

--- src/containers/BurgerBuilder/BurgerIngredient.js
+++ src/containers/BurgerBuilder/BurgerIngredient.js
@@ -2,16 +2,20 @@ import React from 'react'
 
 import BreadTop from './ingredients/BreadTop'
 import Cheese from './ingredients/Cheese'
+import Salad from './ingredients/Salad'
 
 const BurgerIngredient = (props) => {
   let ingredient = null
 
   switch (props.type) {
     case 'bread-top':
-      ingredient = <div></div>
+      ingredient = <BreadTop />
       break
     case 'salad':
-      ingredient = <div></div>
+      ingredient = <Salad />
+      break
+    case 'cheese':
+      ingredient = <Cheese />
       break
     case 'bread-bottom':
       ingredient = <div></div>
@@ -21,8 +25,7 @@ const BurgerIngredient = (props) => {
       break
   }
 
-  return <div>
-  </div>
+  return <div></div>
 }
 
 export default BurgerIngredient
